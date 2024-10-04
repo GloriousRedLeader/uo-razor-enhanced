ad7ddea68900eb3259acce61bb01db9ebfc930da
GloriousRedLeader <arturotheburro7@gmail.com>
2024-10-04 12:10:32 -0700

    updates to boss alert

--- fm_tools/JournalAlert.py
+++ fm_tools/JournalAlert.py
@@ -4,11 +4,11 @@ Player.HeadMessage(58, "SoTW boss scanner is running!")
 
 while True:
     Misc.Pause(1000)
-    if Journal.Search("The Master of The Hunt has") or Journal.Search("Freya has"):
+    if Journal.Search("The Master of The Hunt has") or Journal.Search("Freyr has"):
         Journal.Clear()
         for i in range(0, HOW_MANY_TIMES_TO_BEEP):
             Misc.Beep()
             Player.HeadMessage( 28, '^^ Boss has spawned (somewhere) ^^' )
             Player.HeadMessage( 38, '^^ Boss has spawned (somewhere) ^^' )
             Player.HeadMessage( 48, '^^ Boss has spawned (somewhere) ^^' )
-            Misc.Pause(1000)         
+            Misc.Pause(1000) 
