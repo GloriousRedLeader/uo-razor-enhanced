169a4423a6d12e9c678ddd40ca93c3f8678858e0
GloriousRedLeader <arturotheburro7@gmail.com>
2024-10-03 18:11:34 -0700

    adding other boss

--- fm_tools/JournalAlert.py
+++ fm_tools/JournalAlert.py
@@ -1,10 +1,14 @@
+HOW_MANY_TIMES_TO_BEEP = 5
+
+Player.HeadMessage(58, "SoTW boss scanner is running!")
+
 while True:
     Misc.Pause(1000)
-    if Journal.Search("The Master of The Hunt has"):
+    if Journal.Search("The Master of The Hunt has") or Journal.Search("Freya has"):
         Journal.Clear()
-        for i in range(0, 3):
+        for i in range(0, HOW_MANY_TIMES_TO_BEEP):
             Misc.Beep()
-            Player.HeadMessage( 28, '^^ Master of the Cunt spawned ^^ ' )
-            Player.HeadMessage( 28, '^^ Master of the Cunt spawned ^^ ' )
-            Player.HeadMessage( 28, '^^ Master of the Cunt spawned ^^ ' )
+            Player.HeadMessage( 28, '^^ Boss has spawned (somewhere) ^^' )
+            Player.HeadMessage( 38, '^^ Boss has spawned (somewhere) ^^' )
+            Player.HeadMessage( 48, '^^ Boss has spawned (somewhere) ^^' )
             Misc.Pause(1000)         
