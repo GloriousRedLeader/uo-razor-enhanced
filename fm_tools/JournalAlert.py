4605c19b0e433a946e85bfafb88d18344fb86a65
GloriousRedLeader <arturotheburro7@gmail.com>
2024-10-15 00:37:22 -0700

    journal alert

--- fm_tools/JournalAlert.py
+++ fm_tools/JournalAlert.py
@@ -1,25 +1,41 @@
+# Razor Enhanced Scripts for Ultima Online by
+#   GRL  
+#   https://github.com/GloriousRedLeader/uo-razor-enhanced
+#   2024-03-26
+# Use at your own risk. 
+
+# This is a standalone script that will monitor exciting entries in the journal.
+# If you're looking for a rare pet, just plug the name in here.
+# If you're waiting for a boss to spawn during some event (e.g. SoTW on InsaneUO, 
+# add the strings here.
+
+# Configs
 HOW_MANY_TIMES_TO_BEEP = 5
-
-Player.HeadMessage(58, "SoTW boss scanner is running!")
-# Real string in game: The Master of the Hunt has arrived to the South-East...
-
-strings = [
+HOW_OFTEN_TO_PING_MS = 3000
+STRINGS_TO_LOOK_FOR = [
     "The Master of the Hunt has",
     "You sense a dark presence",
     "a putrid steed",
     "a venom steed"
 ]
 
+Timer.Create( 'journalAlertPingTimer', 1 )
+
 Journal.Clear()
 while True:
-    Misc.Pause(1000)
-    for search in strings:
-        #if Journal.Search("The Master of the Hunt has") or Journal.Search("You sense a dark presence"):
+    
+    if Timer.Check( 'journalAlertPingTimer' ) == False:
+        Player.HeadMessage( 58, "Journal Alert Running...")
+        Timer.Create( 'journalAlertPingTimer', HOW_OFTEN_TO_PING_MS )
+    
+    for search in STRINGS_TO_LOOK_FOR:
         if Journal.Search(search):
             Journal.Clear()
             for i in range(0, HOW_MANY_TIMES_TO_BEEP):
                 Misc.Beep()
-                Player.HeadMessage( 28, '^^ Boss has spawned (somewhere) ^^' )
-                Player.HeadMessage( 38, '^^ Boss has spawned (somewhere) ^^' )
-                Player.HeadMessage( 48, '^^ Boss has spawned (somewhere) ^^' )
+                Player.HeadMessage( 28, '^^ Journal Alert ^^' )
+                Player.HeadMessage( 38, '^^ Journal Alert ^^' )
+                Player.HeadMessage( 48, '^^ Journal Alert ^^' )
                 Misc.Pause(1000) 
+
+    Misc.Pause(1000)      
