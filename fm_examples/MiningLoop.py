9b3ee3bf8baae79f8ee321a6bc4f5cb904af1408
GloriousRedLeader <arturotheburro7@gmail.com>
2024-11-08 11:58:42 -0800

    testing

---
+++ fm_examples/MiningLoop.py
@@ -0,0 +1,9 @@
+from Scripts.fm_core.core_gathering import run_mining_loop
+
+run_mining_loop(
+    # Required. Your fire beetle's name.
+    forgeAnimalName = "fire beetle name",
+    
+    # Required. One or more blue beetle names.
+    packAnimalNames = ["your blue beetle name", "your other blue beetle name"]
+)
