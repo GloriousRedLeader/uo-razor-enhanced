c99e4c92cac5889e17c84b40bdcc6f098153ebf9
GloriousRedLeader <arturotheburro7@gmail.com>
2025-01-06 11:24:05 -0800

    testing

--- fm_examples/FishLoop.py
+++ fm_examples/FishLoop.py
@@ -1,4 +1,20 @@
 from Scripts.fm_core.core_gathering import run_fishing_loop
-from Scripts.fm_core.core_rails import get_tile_in_front
-   
-run_fishing_loop(fishRange = 4, moveTiles = 0, fishDelayMs = 10000)
+
+run_fishing_loop(
+    # How many tiles in front of character to fish
+    fishRange = 4, 
+    
+    # If on a boat, tells the tiller to move forward this many times.
+    moveTiles = 2, 
+    
+    # How long to pause between casts
+    fishDelayMs = 10000,
+    
+    # 0 = Do nothing, leave in backpack
+    # 1 = cut fish with dagger to reduce weight, makes lots of fish steaks
+    # 2 = place fish in cargo hold of ship, have to be standing near cargo hold
+    fishHandling = 0,
+    
+    # Will not do any fishHandling operations on this fish. Leaves it in backpack. Useful for fishing quests.
+    fishToKeep = None
+)
